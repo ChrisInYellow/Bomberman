13b73bbf1359abca50b643814dfba99970a7001b
ChrisInYellow <chris_albin@outlook.com>
2018-03-06 08:45:50 +0100

    Uploading project

---
+++ BombermanStarterProject/Bomberman Starter Project/Assets/Scripts/BombSpawner.cs
@@ -0,0 +1,27 @@
+﻿
+using UnityEngine;
+using UnityEngine.Tilemaps; 
+
+public class BombSpawner : MonoBehaviour {
+
+    public Tilemap tilemap;
+
+    public GameObject bomb; 
+	
+	// Update is called once per frame
+	void Update () {
+        SpawnBomb(); 
+	}
+
+    public void SpawnBomb()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Vector3 spawningPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector3Int cell = tilemap.WorldToCell(spawningPos);
+            Vector3 cellCenter = tilemap.GetCellCenterWorld(cell);
+
+            Instantiate(bomb, cellCenter, Quaternion.identity); 
+        }
+    }
+}
---
+++ BombermanStarterProject/Bomberman Starter Project/Assets/TileRemover.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class TileRemover : MonoBehaviour
+{
+
+    public Tilemap tilemap;
+    public Tile wallTile;
+    public Tile destructibleTile;
+    public GameObject explosionPrefab;
+
+
+
+    public void Explosion(Vector2 worldPos)
+    {
+        Vector3Int originCell = tilemap.WorldToCell(worldPos);
+
+        BlastRadius(originCell);
+        if (BlastRadius(originCell + new Vector3Int(1, 0, 0)))
+        {
+            BlastRadius(originCell + new Vector3Int(2, 0, 0));
+        }
+
+        if (BlastRadius(originCell + new Vector3Int(0, 1, 0)))
+        {
+            BlastRadius(originCell + new Vector3Int(0, 2, 0));
+        }
+
+        if (BlastRadius(originCell + new Vector3Int(-1, 0, 0)))
+        {
+            BlastRadius(originCell + new Vector3Int(-2, 0, 0));
+        }
+
+        if (BlastRadius(originCell + new Vector3Int(0, -1, 0)))
+        {
+            BlastRadius(originCell + new Vector3Int(0, -2, 0));
+        }
+    }
+    private bool BlastRadius(Vector3Int cell)
+    {
+        Tile tile = tilemap.GetTile<Tile>(cell);
+
+        if (tile == wallTile)
+        {
+            return false;
+        }
+
+        if (tile == destructibleTile)
+        {
+            tilemap.SetTile(cell, null);
+        }
+
+        Vector3 pos = tilemap.GetCellCenterWorld(cell);
+        Instantiate(explosionPrefab, pos, Quaternion.identity);
+
+        return true;
+    }
+}
